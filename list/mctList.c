757327e49c44af17f582728beda32db0e28bc0d3
fuxiaoY <14094535+fuxiaoy@user.noreply.gitee.com>
2024-12-04 18:01:32 +0800

    【list】:增加接口

--- list/mctList.c
+++ list/mctList.c
@@ -6,7 +6,7 @@ const tModemList modemList[] =
     {
         {.name = "CONSOLE",    .api = mctConsoleApiGet},
         //{.name = "A7680C",      .api = CMD_A7680CApiGet},
-
+        //{.name = "Y7025",        .api = CMD_Y7025ApiGet},
 
 };
 uint16_t mctModemLisNumGet(void)
