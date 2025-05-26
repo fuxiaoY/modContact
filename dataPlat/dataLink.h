a990514a6aaf95f38e6a1398b2cb5306128ad3d6
fuxiaoY <18280138746@163.com>
2025-05-26 18:43:13 +0800

    【格式】：数据对齐

--- dataPlat/dataLink.h
+++ dataPlat/dataLink.h
@@ -11,29 +11,29 @@ extern "C" {
 
 // INDEX名称  数据指针  数据长度
 #define MCT_DATA_LIST \
-    X(MCT(devName),global_cfg.DevName, sizeof(global_cfg.DevName)) \
-    X(MCT(serverURL),global_cfg.ServerURL, sizeof(global_cfg.ServerURL)) \
-    X(MCT(serverPort),&global_cfg.ServerPort, sizeof(global_cfg.ServerPort)) \
-    X(MCT(updateURL),global_cfg.UpdateURL, sizeof(global_cfg.UpdateURL)) \
-    X(MCT(updatePort),&global_cfg.UpdatePort, sizeof(global_cfg.UpdatePort)) \
-    X(MCT(mqttUploadTopic),g_MQTopic.Send, sizeof(g_MQTopic.Send)) \
-    X(MCT(mqttRevTopic),g_MQTopic.Recv, sizeof(g_MQTopic.Recv)) \
-    X(MCT(mqttRspTopic),g_MQTopic.Resp, sizeof(g_MQTopic.Resp)) \
-    X(MCT(mqttCfgUpTopic),g_MQTopic.ConfigUp, sizeof(g_MQTopic.ConfigUp)) \
-    X(MCT(mqttCfgDnTopic),g_MQTopic.ConfigDn, sizeof(g_MQTopic.ConfigDn)) \
-    X(MCT(mqttProductKey),global_cfg.ProductKey, sizeof(global_cfg.ProductKey)) \
-    X(MCT(mqttDevSecret),global_cfg.DevSecret, sizeof(global_cfg.DevSecret)) \
-    X(MCT(mqttKeepAlive),&global_cfg.KeepAlive, sizeof(global_cfg.KeepAlive)) \
-    X(MCT(mqttConnectTimeout),&global_cfg.ConnectTimeOut, sizeof(global_cfg.ConnectTimeOut)) \
-    X(MCT(imsi),global_state.wan_imsi, sizeof(global_state.wan_imsi)) \
-    X(MCT(imei),global_state.wan_imei, sizeof(global_state.wan_imei)) \
-    X(MCT(ver),global_state.wan_ver, sizeof(global_state.wan_ver)) \
-    X(MCT(iccid),global_state.wan_iccid, sizeof(global_state.wan_iccid)) \
-    X(MCT(signal),&networkPara.SignalStrength, sizeof(networkPara.SignalStrength)) \
-    X(MCT(netStatus),&networkPara.REGstatus, sizeof(networkPara.REGstatus)) \
-    X(MCT(mqttWanBuf),mqttWanBuf, sizeof(mqttWanBuf)) \
-    X(MCT(mqttWanBufReady),&mqttWanBufReady, sizeof(mqttWanBufReady)) \
-    X(MCT(mqttWanPub),&mqttWanPub, sizeof(mqttWanPub)) \
+    X(MCT(devName),           global_cfg.DevName,         sizeof(global_cfg.DevName))         \
+    X(MCT(serverURL),         global_cfg.ServerURL,       sizeof(global_cfg.ServerURL))       \
+    X(MCT(serverPort),        &global_cfg.ServerPort,     sizeof(global_cfg.ServerPort))      \
+    X(MCT(updateURL),         global_cfg.UpdateURL,       sizeof(global_cfg.UpdateURL))       \
+    X(MCT(updatePort),        &global_cfg.UpdatePort,     sizeof(global_cfg.UpdatePort))      \
+    X(MCT(mqttUploadTopic),   g_MQTopic.Send,             sizeof(g_MQTopic.Send))             \
+    X(MCT(mqttRevTopic),      g_MQTopic.Recv,             sizeof(g_MQTopic.Recv))             \
+    X(MCT(mqttRspTopic),      g_MQTopic.Resp,             sizeof(g_MQTopic.Resp))             \
+    X(MCT(mqttCfgUpTopic),    g_MQTopic.ConfigUp,         sizeof(g_MQTopic.ConfigUp))         \
+    X(MCT(mqttCfgDnTopic),    g_MQTopic.ConfigDn,         sizeof(g_MQTopic.ConfigDn))         \
+    X(MCT(mqttProductKey),    global_cfg.ProductKey,      sizeof(global_cfg.ProductKey))      \
+    X(MCT(mqttDevSecret),     global_cfg.DevSecret,       sizeof(global_cfg.DevSecret))       \
+    X(MCT(mqttKeepAlive),     &global_cfg.KeepAlive,      sizeof(global_cfg.KeepAlive))       \
+    X(MCT(mqttConnectTimeout),&global_cfg.ConnectTimeOut, sizeof(global_cfg.ConnectTimeOut))  \
+    X(MCT(imsi),              global_state.wan_imsi,      sizeof(global_state.wan_imsi))      \
+    X(MCT(imei),              global_state.wan_imei,      sizeof(global_state.wan_imei))      \
+    X(MCT(ver),               global_state.wan_ver,       sizeof(global_state.wan_ver))       \
+    X(MCT(iccid),             global_state.wan_iccid,     sizeof(global_state.wan_iccid))     \
+    X(MCT(signal),            &networkPara.SignalStrength,sizeof(networkPara.SignalStrength)) \
+    X(MCT(netStatus),         &networkPara.REGstatus,     sizeof(networkPara.REGstatus))      \
+    X(MCT(mqttWanBuf),        mqttWanBuf,                 sizeof(mqttWanBuf))                 \
+    X(MCT(mqttWanBufReady),   &mqttWanBufReady,           sizeof(mqttWanBufReady))            \
+    X(MCT(mqttWanPub),        &mqttWanPub,                sizeof(mqttWanPub))                 \
 // 生成枚举索引
 typedef enum {
     #undef X
